7357f55a766c1795cd34d76e88ae0c1c73d9333b
Mano <mano-bhargav-reddy.dodla@capgemini.com>
2020-03-28 18:12:41 -0400

    Checking for git commit

--- src/main/scala/com/coke/nsrV3/SparkBasic.scala
+++ src/main/scala/com/coke/nsrV3/SparkBasic.scala
@@ -9,7 +9,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
-class SparkBasic {
+object SparkBasic extends App {
 
     System.setProperty("hadoop.home.dir","c:/winutils")
     val spark = SparkSession
--- src/main/scala/com/coke/nsrV3/commons/CommonFunctions.scala
+++ src/main/scala/com/coke/nsrV3/commons/CommonFunctions.scala
@@ -5,30 +5,58 @@ import java.sql.DriverManager
 import org.apache.log4j.{BasicConfigurator, Level, Logger}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-object CommonFunctions extends App {
+object CommonFunctions {
+
+  def main(args: Array[String]): Unit = {
+
+/*    val dataFile = loadData("Connection URL","Database table","Username","Password")
+    showData(dataFile)
+    printSchema(dataFile)
+    isNull(dataFile)*/
+
+
+    val logger = Logger.getLogger(this.getClass.getName)
+
+
+    System.setProperty("hadoop.home.dir","c:/winutils")
+    val spark = SparkSession.builder().master("local[*]")
+      .appName("Database connection").getOrCreate()
+/*    logger.info("Spark intialized")
+    logger.info("Git trail")
+    logger.info("Again updating")
+    val inputdata1 = spark.read.csv("file:///C:/revenue_shipfrom.csv")
+    logger.info("Data loaded")
+    inputdata1.show()*/
+println("The header value is  "+headerCheck("andina-rp_yyyymmdd_hhmmss_revenue_customer.csv"))
+
+
+    def headerCheck(file:String):Boolean = {
+      val config_file = spark.read.csv("file:///C:/config.csv")
+
+      config_file.createOrReplaceTempView("configtable")
+     // config_file.show()
+
+      val headertable = spark.sql("select _c5 as filename,_c2 as header from configtable")
+      headertable.show()
+      headertable.createOrReplaceTempView("headerboolean")
+
+      val headerbool = spark.sql("select header from headerboolean where filename = '{}'".format(file))
+      if (headerbool == 'Y') true
+      else false
 
 
 
-  //val dataFile = loadData("Connection URL","Database table","Username","Password")
-  //showData(dataFile)
-  //printSchema(dataFile)
-  //isNull(dataFile)
 
 
-  val logger = Logger.getLogger(this.getClass.getName)
+
+    }
 
 
-  System.setProperty("hadoop.home.dir","c:/winutils")
-  val spark = SparkSession.builder().master("local[*]")
-    .appName("Database connection").getOrCreate()
-  logger.info("Spark intialized")
-  logger.info("Git trail")
-  logger.info("Again updating")
-  val inputdata1 = spark.read.csv("file:///C:/revenue_shipfrom.csv")
-  logger.info("Data loaded")
-  inputdata1.show()
 
 
+
+
+  }
 
 
   def loadData(url:String,table:String,username:String,Password:String):DataFrame = {
@@ -68,5 +96,7 @@ object CommonFunctions extends App {
 
 
   }
+
+
 
 }
